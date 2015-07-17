e1e59ded49367b1a9c72300a3835567a975a648c
Alex White <awhit012@gmail.com>
2015-07-17 15:29:11 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Artspot::Application.config.secret_token = 'c5a8a2b8ebd40496a22e6069172152612b6d629a20d15f5688ad6fc8c374297cac50d44f9c4a450ffd70c714246a4f9c0cf2d9c4d0f982e42eb753c5f7840267'
