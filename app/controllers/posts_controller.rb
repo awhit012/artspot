e43a93919d9a5d6fe2c0b6f0bd9f3e690608b9a0
Alex White <awhit012@gmail.com>
2015-07-20 15:12:04 -0700

    Users can only edit or delete their own even if they go to the route themeselves

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_filter :authenticate_user!, :set_post, only: [:show, :edit, :update, :destroy]
+  before_filter :owned_post, only: [:edit, :update, :destroy]
 
   def index
     @posts = Post.all
@@ -49,6 +50,13 @@ class PostsController < ApplicationController
 
   def set_post
     @post = Post.find(params[:id])
+  end
+
+  def owned_post
+    unless current_user == @post.user
+      flash[:alert] = "That post doesn't belong to you!"
+      redirect_to root_path
+    end
   end
 
 end
