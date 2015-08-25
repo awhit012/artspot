d5bce3a2b61033e83526a387fbbefdd0be70736b
Alex White <awhit012@gmail.com>
2015-08-25 13:48:12 -0700

    configures imageMagick

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Artspot::Application.initialize!
+
+Paperclip.options[:image_magick_path] = "/usr/local/bin"
+Paperclip.options[:command_path] = "/usr/local/bin"
